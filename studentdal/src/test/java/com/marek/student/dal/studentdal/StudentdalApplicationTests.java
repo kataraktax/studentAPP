5f6375bcef1a4d04a7c1d8fdfbd0eb020765391a
kataraktax <marek.biedrzycki1984@gmail.com>
2020-02-09 23:43:26 +0100

    creating project

---
+++ studentdal/src/test/java/com/marek/student/dal/studentdal/StudentdalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marek.student.dal.studentdal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentdalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
